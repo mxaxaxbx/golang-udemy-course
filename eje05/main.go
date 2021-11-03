e4b10bc40818b4735b3cf93b9bfecf90eb4a3e22
Miguel Arenas <miguel.arenas@sitidoctor.com>
2021-11-02 21:07:50 -0500

    loops

---
+++ eje05/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var i int = 0
+
+RUTINA:
+	for i < 10 {
+		if i == 4 {
+			i = i + 2
+			fmt.Println("voy a rutina sumando 2 a i")
+			goto RUTINA
+		}
+		fmt.Printf("Valor de i: %d \n ", i)
+		i++
+	}
+}
